5a3edb0441ea542dedce09a454a306cbd5ac717d
kplnhsyn <kplnhsyn.49@gmail.com>
2022-07-13 16:16:47 +0200

    end

--- clarusway_fs/react/projects/projects/002-Language-Cards-RP-02/solution/src/components/item/Item.js
+++ clarusway_fs/react/projects/projects/002-Language-Cards-RP-02/solution/src/components/item/Item.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import "./Item.css";
+import { useState } from 'react';
+import './Item.css';
 
 const Item = ({ card }) => {
   const [showLogo, setShowLogo] = useState(true);
