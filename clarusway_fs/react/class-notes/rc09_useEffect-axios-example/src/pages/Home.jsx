bf81194ef4bff6f2dc936d201205414c2230fbef
kplnhsyn <kplnhsyn.49@gmail.com>
2022-07-19 18:33:06 +0200

    movie-app

--- clarusway_fs/react/class-notes/rc09_useEffect-axios-example/src/pages/Home.jsx
+++ clarusway_fs/react/class-notes/rc09_useEffect-axios-example/src/pages/Home.jsx
@@ -1,14 +1,89 @@
-import AddTutorial from '../components/AddTutorial';
-import TutorialList from '../components/TutorialList';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+// import AddTutorial from "../components/AddTutorial";
+// import TutorialList from "../components/TutorialList";
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+
+// const Home = () => {
+//   const [tutorials, setTutorials] = useState();
+
+//   const url = "https://tutorials-api-cw.herokuapp.com/api/tutorials";
+
+//   //! GET (Read)
+//   const getTutorials = async () => {
+//     try {
+//       const { data } = await axios.get(url);
+//       setTutorials(data);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   //? Sadece Component mount oldugunda istek yapar
+//   useEffect(() => {
+//     getTutorials();
+//   }, []);
+
+//   console.log(tutorials);
+
+//   //! POST (Create)
+//   const addTutorial = async (tutorial) => {
+//     try {
+//       await axios.post(url, tutorial);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     getTutorials();
+//   };
+
+//   //! DELETE (delete)
+//   const deleteTutorial = async (id) => {
+//     try {
+//       await axios.delete(`${url}/${id}`);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     getTutorials();
+//   };
+
+//   //! Update (PUT:Whole Update,PATCH :Partially Update)
+//   const editTutorial = async (id, title, desc) => {
+//     const filtered = tutorials
+//       .filter((tutor) => tutor.id === id)
+//       .map(() => ({ title: title, description: desc }));
+
+//     console.log(filtered);
+//     try {
+//       await axios.put(`${url}/${id}`, filtered[0]);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     getTutorials();
+//   };
+
+//   return (
+//     <>
+//       <AddTutorial addTutorial={addTutorial} />
+//       <TutorialList
+//         tutorials={tutorials}
+//         deleteTutorial={deleteTutorial}
+//         editTutorial={editTutorial}
+//       />
+//     </>
+//   );
+// };
+
+// export default Home;
+
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import React from "react";
 
 const Home = () => {
   const [tutorials, setTutorials] = useState();
-
-  const url = 'https://tutorials-api-cw.herokuapp.com/api/tutorials';
-
-  //! GET (Read)
+  const url = "";
   const getTutorials = async () => {
     try {
       const { data } = await axios.get(url);
@@ -17,15 +92,10 @@ const Home = () => {
       console.log(error);
     }
   };
-
-  //? Sadece Component mount oldugunda istek yapar
   useEffect(() => {
     getTutorials();
   }, []);
 
-  console.log(tutorials);
-
-  //! POST (Create)
   const addTutorial = async (tutorial) => {
     try {
       await axios.post(url, tutorial);
@@ -35,40 +105,11 @@ const Home = () => {
     getTutorials();
   };
 
-  //! DELETE (delete)
-  const deleteTutorial = async (id) => {
-    try {
-      await axios.delete(`${url}/${id}`);
-    } catch (error) {
-      console.log(error);
-    }
-    getTutorials();
-  };
-
-  //! Update (PUT:Whole Update,PATCH :Partially Update)
-  const editTutorial = async (id, title, desc) => {
-    const filtered = tutorials
-      .filter((tutor) => tutor.id === id)
-      .map(() => ({ title: title, description: desc }));
-
-    console.log(filtered);
-    try {
-      await axios.put(`${url}/${id}`, filtered[0]);
-    } catch (error) {
-      console.log(error);
-    }
-    getTutorials();
-  };
-
   return (
-    <>
+    <div>
       <AddTutorial addTutorial={addTutorial} />
-      <TutorialList
-        tutorials={tutorials}
-        deleteTutorial={deleteTutorial}
-        editTutorial={editTutorial}
-      />
-    </>
+      <TutorialList tutorials={tutorials} />
+    </div>
   );
 };
 
